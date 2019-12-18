5894e1a2a42ff0f27995a1c1aa4991303687cb1f
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-12-18 13:21:54 +0100

    feat: resolve when action creator is triggered

--- test/asyncAction.test.ts
+++ test/asyncAction.test.ts
@@ -2,13 +2,15 @@ import {
   configureStore,
   lifeCredentialsSelector,
   putCredentials,
+  setOAuth2,
 } from 'ts-foursquare'
 import { dispatchActionsAndWaitResponse } from '../src'
 
 describe('asyncAction', () => {
   it(`should wait end of putCredentials with promise and make a snapshot of store`, done => {
     dispatchActionsAndWaitResponse({
-      actions: [],
+      actionsDispatch: [],
+      actionCreatorsResolve: [],
       store: configureStore(),
       selector: () => {},
     }).catch(error => {
@@ -19,12 +21,18 @@ describe('asyncAction', () => {
 
   it(`should wait end of putCredentials with promise and make a snapshot of store`, done => {
     dispatchActionsAndWaitResponse({
-      actions: [
+      actionsDispatch: [
         putCredentials({
           clientId: '123',
           clientSecret: '456',
         }),
+        setOAuth2({
+          clientId: '123',
+          clientSecret: '456',
+          redirectUri: 'https://www.google.com',
+        }),
       ],
+      actionCreatorsResolve: [setOAuth2],
       store: configureStore(),
       selector: lifeCredentialsSelector,
     }).then(data => {
